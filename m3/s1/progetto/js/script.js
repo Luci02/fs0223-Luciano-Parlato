3fb2b05765608e58cf48f32b87e1d04bad034988
Luci02 <luciparlato50@gmail.com>
2023-05-27 18:02:20 +0200

    commit

--- m3/s1/progetto/js/script.js
+++ m3/s1/progetto/js/script.js
@@ -1,11 +1,4 @@
 "use strict";
-class RegistroChiamate {
-    constructor(obj) {
-        this.id = obj.id;
-        this.durata = obj.durata;
-        this.dataOra = obj.dataOra;
-    }
-}
 class Smartphone {
     constructor() {
         this.carica = 5;
@@ -15,13 +8,12 @@ class Smartphone {
     }
     ricarica(euro) {
         this.carica += euro;
-        console.log('La tua ricarica di ' + euro + '€ è avvenuta con successo');
+        console.log('La tua ricarica da ' + euro + '€ è avvenuta con successo');
     }
     numero404() {
         return 'Il credito residuo è: ' + this.carica.toFixed(2) + '€';
     }
     getNumeroChiamate() {
-        console.log(`Hai effettuato ${this.numeroChiamate} chiamate`);
         return this.numeroChiamate;
     }
     chiamata(min) {
@@ -30,13 +22,16 @@ class Smartphone {
             this.numeroChiamate++;
             // riduco il credito
             this.carica -= (min * this.costoMinuto);
-            console.log('il costo della chiamata è stato di ', (min * this.costoMinuto));
-            // Aggiungo la chiamata al registro
-            this.registroChiamate.push({
+            console.log('Hai effettuato una chiamata durata', min, 'min');
+            console.log('Il costo della chiamata è stato di', (min * this.costoMinuto) + '€');
+            // Creo un oggetto da inserire nell'array
+            let chiamata = {
                 id: this.registroChiamate.length + 1,
                 durata: min,
-                dataOra: new Date()
-            });
+                dataOra: new Date(),
+            };
+            // Aggiungo la chiamata al registro
+            this.registroChiamate.push(chiamata);
         }
         else {
             console.log('Non hai credito sufficiente per effettuare la chiamata');
@@ -46,13 +41,11 @@ class Smartphone {
         this.numeroChiamate = 0;
     }
     mostraRegistroChiamate() {
-        console.log(this.registroChiamate);
         this.registroChiamate.forEach((chiamata) => {
-            var _a, _b;
             console.log('Hai effettuato una chiamata il giorno: '
-                + ((_a = chiamata.dataOra) === null || _a === void 0 ? void 0 : _a.toLocaleDateString('it-IT'))
+                + chiamata.dataOra.toLocaleDateString('it-IT')
                 + ' alle ore '
-                + ((_b = chiamata.dataOra) === null || _b === void 0 ? void 0 : _b.toLocaleTimeString('it-IT'))
+                + chiamata.dataOra.toLocaleTimeString('it-IT')
                 + ' che è durata '
                 + chiamata.durata
                 + ' minuti.');
@@ -60,8 +53,7 @@ class Smartphone {
     }
     filtraChiamatePerDataOra(data) {
         return this.registroChiamate.filter((element) => {
-            var _a;
-            if (((_a = element.dataOra) === null || _a === void 0 ? void 0 : _a.toLocaleDateString('it-IT')) == (data === null || data === void 0 ? void 0 : data.toLocaleDateString('it-IT'))) {
+            if (element.dataOra.toLocaleDateString('it-IT') == data.toLocaleDateString('it-IT')) {
                 return element;
             }
         });
@@ -71,44 +63,48 @@ let firstUser = new Smartphone();
 let secondUser = new Smartphone();
 let thirdUser = new Smartphone();
 // Primo utente
-console.log(firstUser.numero404());
 firstUser.ricarica(5);
 console.log(firstUser.numero404());
-firstUser.getNumeroChiamate();
-firstUser.chiamata(1);
-firstUser.getNumeroChiamate();
+console.log('Il numero di chiamate fino ad ora è di:', firstUser.getNumeroChiamate(), 'chiamate');
 firstUser.chiamata(5);
-firstUser.chiamata(4);
+firstUser.chiamata(1);
+firstUser.chiamata(10);
+firstUser.chiamata(100);
 console.log(firstUser.numero404());
+console.log('Il numero di chiamate fino ad ora è di:', firstUser.getNumeroChiamate(), 'chiamate');
+console.log('Chiamate azzerate');
 firstUser.azzeraChiamate();
-firstUser.getNumeroChiamate();
+console.log('Il numero di chiamate fino ad ora è di:', firstUser.getNumeroChiamate(), 'chiamate');
 firstUser.mostraRegistroChiamate();
-console.log(firstUser.filtraChiamatePerDataOra(new Date('2023-05-26')));
+console.log(firstUser.filtraChiamatePerDataOra(new Date('2023/05/27')));
 // Secondo utente
-console.log(secondUser.numero404());
 secondUser.ricarica(5);
 console.log(secondUser.numero404());
-secondUser.getNumeroChiamate();
-secondUser.chiamata(1);
-secondUser.getNumeroChiamate();
+console.log('Il numero di chiamate fino ad ora è di:', secondUser.getNumeroChiamate(), 'chiamate');
 secondUser.chiamata(5);
-secondUser.chiamata(4);
+secondUser.chiamata(1);
+secondUser.chiamata(10);
+secondUser.chiamata(100);
 console.log(secondUser.numero404());
+console.log('Il numero di chiamate fino ad ora è di:', secondUser.getNumeroChiamate(), 'chiamate');
+console.log('Chiamate azzerate');
 secondUser.azzeraChiamate();
-secondUser.getNumeroChiamate();
+console.log('Il numero di chiamate fino ad ora è di:', secondUser.getNumeroChiamate(), 'chiamate');
 secondUser.mostraRegistroChiamate();
-console.log(secondUser.filtraChiamatePerDataOra(new Date('2023-05-26')));
+console.log(secondUser.filtraChiamatePerDataOra(new Date('2023/05/27')));
 // Terzo utente
-console.log(thirdUser.numero404());
 thirdUser.ricarica(5);
 console.log(thirdUser.numero404());
-thirdUser.getNumeroChiamate();
-thirdUser.chiamata(1);
-thirdUser.getNumeroChiamate();
+console.log('Il numero di chiamate fino ad ora è di:', thirdUser.getNumeroChiamate(), 'chiamate');
 thirdUser.chiamata(5);
-thirdUser.chiamata(4);
+thirdUser.chiamata(1);
+thirdUser.chiamata(10);
+thirdUser.chiamata(100);
 console.log(thirdUser.numero404());
+console.log('Il numero di chiamate fino ad ora è di:', thirdUser.getNumeroChiamate(), 'chiamate');
+console.log('Chiamate azzerate');
 thirdUser.azzeraChiamate();
-thirdUser.getNumeroChiamate();
+console.log('Il numero di chiamate fino ad ora è di:', thirdUser.getNumeroChiamate(), 'chiamate');
 thirdUser.mostraRegistroChiamate();
-console.log(thirdUser.filtraChiamatePerDataOra(new Date('2023-05-26')));
+console.log(thirdUser.filtraChiamatePerDataOra(new Date('2023/05/27')));
+//# sourceMappingURL=script.js.map
--- m3/s1/progetto/script.ts
+++ m3/s1/progetto/script.ts
@@ -12,28 +12,17 @@ interface ISmartphone{
 
 }
 
-class RegistroChiamate{
-
-    id: number;
-    durata?: number;
-    dataOra?: Date;
-
-    constructor(obj:{
-        id:number;
-        durata:number;
-        dataOra:Date;
-    }){
-        this.id = obj.id;
-        this.durata = obj.durata;
-        this.dataOra = obj.dataOra;
-    }
+interface IRegistroChiamate{
+    id:number;
+    durata: number;
+    dataOra: Date;
 }
 
 class Smartphone implements ISmartphone{
     carica: number;
     numeroChiamate: number;
     costoMinuto: number;
-    registroChiamate: RegistroChiamate[];
+    registroChiamate: IRegistroChiamate[];
 
     constructor(){
         this.carica = 5;
@@ -44,7 +33,7 @@ class Smartphone implements ISmartphone{
 
     ricarica(euro: number): void {
         this.carica += euro;
-        console.log('La tua ricarica di ' + euro + '€ è avvenuta con successo');
+        console.log('La tua ricarica da ' + euro + '€ è avvenuta con successo');
     }
 
     numero404(): string {
@@ -52,7 +41,6 @@ class Smartphone implements ISmartphone{
     }
 
     getNumeroChiamate(): number {
-        console.log(`Hai effettuato ${this.numeroChiamate} chiamate`);
         return this.numeroChiamate;
     }
 
@@ -65,14 +53,18 @@ class Smartphone implements ISmartphone{
 
             // riduco il credito
             this.carica -= (min * this.costoMinuto);
-            console.log('il costo della chiamata è stato di ', (min * this.costoMinuto));
+            console.log('Hai effettuato una chiamata durata', min, 'min');
+            console.log('Il costo della chiamata è stato di', (min * this.costoMinuto) + '€');
 
-            // Aggiungo la chiamata al registro
-            this.registroChiamate.push({
+            // Creo un oggetto da inserire nell'array
+            let chiamata: IRegistroChiamate = {
                 id: this.registroChiamate.length + 1,
                 durata: min,
-                dataOra: new Date()
-            });
+                dataOra: new Date(),
+            }
+            
+            // Aggiungo la chiamata al registro
+            this.registroChiamate.push(chiamata);
 
         }else{
             console.log('Non hai credito sufficiente per effettuare la chiamata');
@@ -87,16 +79,13 @@ class Smartphone implements ISmartphone{
 
     mostraRegistroChiamate(): void {
 
-        console.log(this.registroChiamate);
-        
-
-        this.registroChiamate.forEach( (chiamata:RegistroChiamate) => {
+        this.registroChiamate.forEach( (chiamata:IRegistroChiamate) => {
 
             console.log(
                 'Hai effettuato una chiamata il giorno: '
-                + chiamata.dataOra?.toLocaleDateString('it-IT')
+                + chiamata.dataOra.toLocaleDateString('it-IT')
                 + ' alle ore '
-                + chiamata.dataOra?.toLocaleTimeString('it-IT')
+                + chiamata.dataOra.toLocaleTimeString('it-IT')
                 + ' che è durata '
                 + chiamata.durata
                 + ' minuti.'
@@ -105,12 +94,15 @@ class Smartphone implements ISmartphone{
 
     }
 
-    filtraChiamatePerDataOra( data?:Date ): RegistroChiamate[] {
-        return this.registroChiamate.filter( ( element:RegistroChiamate ) => {
-            if( element.dataOra?.toLocaleDateString('it-IT') == data?.toLocaleDateString('it-IT') ){
+    filtraChiamatePerDataOra( data:Date ): IRegistroChiamate[] {
+
+        return this.registroChiamate.filter( ( element:IRegistroChiamate ) => {
+
+            if( element.dataOra.toLocaleDateString('it-IT') == data.toLocaleDateString('it-IT') ){
                 return element;
             }
-        } )
+
+        })
     }
     
 }
@@ -120,46 +112,52 @@ let secondUser = new Smartphone();
 let thirdUser = new Smartphone();
 
 // Primo utente
-console.log(firstUser.numero404());
 firstUser.ricarica(5);
 console.log(firstUser.numero404());
-firstUser.getNumeroChiamate();
-firstUser.chiamata(1);
-firstUser.getNumeroChiamate();
+console.log('Il numero di chiamate fino ad ora è di:', firstUser.getNumeroChiamate(), 'chiamate');
 firstUser.chiamata(5);
-firstUser.chiamata(4);
+firstUser.chiamata(1); 
+firstUser.chiamata(10);
+firstUser.chiamata(100);
 console.log(firstUser.numero404());
+console.log('Il numero di chiamate fino ad ora è di:', firstUser.getNumeroChiamate(), 'chiamate');
+console.log('Chiamate azzerate');
 firstUser.azzeraChiamate();
-firstUser.getNumeroChiamate();
+console.log('Il numero di chiamate fino ad ora è di:', firstUser.getNumeroChiamate(), 'chiamate');
 firstUser.mostraRegistroChiamate();
-console.log(firstUser.filtraChiamatePerDataOra(new Date('2023-05-26')));
+console.log(firstUser.filtraChiamatePerDataOra(new Date('2023/05/27')));
+
+
+
 
 // Secondo utente
-console.log(secondUser.numero404());
 secondUser.ricarica(5);
 console.log(secondUser.numero404());
-secondUser.getNumeroChiamate();
-secondUser.chiamata(1);
-secondUser.getNumeroChiamate();
+console.log('Il numero di chiamate fino ad ora è di:', secondUser.getNumeroChiamate(), 'chiamate');
 secondUser.chiamata(5);
-secondUser.chiamata(4);
+secondUser.chiamata(1); 
+secondUser.chiamata(10);
+secondUser.chiamata(100);
 console.log(secondUser.numero404());
+console.log('Il numero di chiamate fino ad ora è di:', secondUser.getNumeroChiamate(), 'chiamate');
+console.log('Chiamate azzerate');
 secondUser.azzeraChiamate();
-secondUser.getNumeroChiamate();
+console.log('Il numero di chiamate fino ad ora è di:', secondUser.getNumeroChiamate(), 'chiamate');
 secondUser.mostraRegistroChiamate();
-console.log(secondUser.filtraChiamatePerDataOra(new Date('2023-05-26')));
+console.log(secondUser.filtraChiamatePerDataOra(new Date('2023/05/27')));
 
 // Terzo utente
-console.log(thirdUser.numero404());
 thirdUser.ricarica(5);
 console.log(thirdUser.numero404());
-thirdUser.getNumeroChiamate();
-thirdUser.chiamata(1);
-thirdUser.getNumeroChiamate();
+console.log('Il numero di chiamate fino ad ora è di:', thirdUser.getNumeroChiamate(), 'chiamate');
 thirdUser.chiamata(5);
-thirdUser.chiamata(4);
+thirdUser.chiamata(1); 
+thirdUser.chiamata(10);
+thirdUser.chiamata(100);
 console.log(thirdUser.numero404());
+console.log('Il numero di chiamate fino ad ora è di:', thirdUser.getNumeroChiamate(), 'chiamate');
+console.log('Chiamate azzerate');
 thirdUser.azzeraChiamate();
-thirdUser.getNumeroChiamate();
+console.log('Il numero di chiamate fino ad ora è di:', thirdUser.getNumeroChiamate(), 'chiamate');
 thirdUser.mostraRegistroChiamate();
-console.log(thirdUser.filtraChiamatePerDataOra(new Date('2023-05-26')));
+console.log(thirdUser.filtraChiamatePerDataOra(new Date('2023/05/27')));
