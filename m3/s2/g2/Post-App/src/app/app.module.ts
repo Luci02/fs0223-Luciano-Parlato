8097947b5bcfa747dd0d91086a08e6f3ab937ecc
Luci02 <luciparlato50@gmail.com>
2023-05-30 19:38:23 +0200

    creato componente inactive posts

--- m3/s2/g2/Post-App/src/app/app.module.ts
+++ m3/s2/g2/Post-App/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ActivePostsComponent } from './pages/active-posts/active-posts.component';
+import { InactivePostsComponent } from './pages/inactive-posts/inactive-posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ActivePostsComponent,
+    InactivePostsComponent
   ],
   imports: [
     BrowserModule,
